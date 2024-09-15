:sparkles: fix sigmoid calculation + add read.md

fee_curves_simulation.py
@@ -1,11 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-# THIS CURVE WILL BE USED FOR CALCULATE THE FEE DELTA IN BPS.
-# MOST LIKELY WE WILL HAVE DIFFERENT CURVES FOR THE INCREASE AND DECREASE FEE.
-# INCREASE ABS - BETWWEN 0 AND ~10%
-# DECREASE ABS - BETWEEN 0 AND BASE FEE (EX: 0.05%)
-
+from scipy.special import expit
 
 # Define the sigmoid function parameters for multiple variations
 # c0 - base fee - for delta P = 0
@@ -19,6 +14,8 @@ params = [
     # {"c0": 0.0, "c1": 1, "c2": 600, "c3": 0.01},
     # {"c0": 0.0, "c1": 1, "c2": 700, "c3": 0.0075},
     {"c0": 0.0, "c1": 1, "c2": 600, "c3": 0.01},
+    {"c0": 0.0, "c1": 1, "c2": 800, "c3": 0.0075},
+    {"c0": 0.0, "c1": 1, "c2": 1000, "c3": 0.005},
     # {"c0": 0.0, "c1": 1, "c2": 600, "c3": 0.01},
     # {"c0": 0.0, "c1": 1, "c2": 700, "c3": 0.01},
     # {"c0": 0.0, "c1": 1, "c2": 700, "c3": 0.0125},
@@ -34,8 +31,8 @@ deltaP = np.linspace(0, 1.0, 500)  # Delta P in percentage decimals from 0% to 1
 
 # Sigmoid S-curve function
 def sigmoid_fee(deltaP, c0, c1, c2, c3):
-    exponent = -c2 * (deltaP - c3)
-    fee = c0 + c1 / (1 + np.exp(exponent))
+    exponent = c2 * (deltaP - c3)
+    fee = c0 + c1 * expit(exponent)
     return fee
 
 
@@ -50,17 +47,10 @@ for i, param in enumerate(params):
         linewidth=2,
     )
 
-# plt.xticks(range(0, 10, 1))
-# plt.yticks(range(0, 22000, 250), fontsize=6)
-# plt.yticks(range(0, 22000, 100), fontsize=6)
-
-# plt.xticks(range(0, 102, 2), fontsize=6)
-
 plt.xlim(0, 6)
-# plt.ylim(0, 2000)
 
 # Plot settings
-plt.title("Sigmoid S-Curve Variations for Abs Delta Swap Fee (%)")
+plt.title("Sigmoid S-Curve Fee Model")
 plt.xlabel("Price Delta (%)")
 plt.ylabel("Abs Delta Swap Fee (%)")
 plt.grid(True)